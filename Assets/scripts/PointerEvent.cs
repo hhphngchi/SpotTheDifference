cec0bd5327b41e2d9fd19cbb6b7bba3cc7975270
hhphngchi <chihoang@berkeley.edu>
2021-02-26 23:15:23 -0800

    commit project

---
+++ Assets/scripts/PointerEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PointerEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+{
+    public Color normalColor = Color.white;
+    public Color enterColor = Color.white;
+    public Color downColor = Color.white;
+    public UnityEvent onClick = new UnityEvent();
+
+    private MeshRenderer mesh = null;
+    void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mesh.material.color = enterColor;
+        print("enter");
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mesh.material.color = normalColor;
+        print("exit");
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        mesh.material.color = downColor;
+        print("down");
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        mesh.material.color = enterColor;
+        print("up");
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClick.Invoke();
+        print("click");
+    }
+}
